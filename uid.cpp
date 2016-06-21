91de75ae420859993030f23cc37de38f5dcaf7d4
Martin Müllenhaupt <muellenhaupt@sikoragmbh.de>
2016-06-21 15:30:01 +0200

    fix CryptoPP::CryptoMaterial::InvalidMaterial exception and catch crypto exceptions

--- uid.cpp
+++ uid.cpp
@@ -61,84 +61,93 @@ int main(int argc, char *argv[])
   //std::cout << root << std::endl;
   std::string json_string = std::string("2") + Json::FastWriter().write(root);
 
-  CryptoPP::AutoSeededRandomPool rng;
+  try
+  {
+    CryptoPP::AutoSeededRandomPool rng;
 
 #define AES_KEY_SIZE 16
 #define AES_BLOCK_SIZE 16
 
-  /* Generate random AES Key and Initialisation vector */
-  CryptoPP::SecByteBlock aes_key( AES_KEY_SIZE );
-  CryptoPP::SecByteBlock iv( AES_BLOCK_SIZE );
+    /* Generate random AES Key and Initialisation vector */
+    CryptoPP::SecByteBlock aes_key( AES_KEY_SIZE );
+    CryptoPP::SecByteBlock iv( AES_BLOCK_SIZE );
 
-  rng.GenerateBlock(aes_key, AES_KEY_SIZE);
-  rng.GenerateBlock(iv, AES_BLOCK_SIZE);
+    rng.GenerateBlock(aes_key, AES_KEY_SIZE);
+    rng.GenerateBlock(iv, AES_BLOCK_SIZE);
 
-  /* Base64 encode IV */
-  std::string iv_b64;
-  {
-    CryptoPP::StringSource s(iv, AES_BLOCK_SIZE, true,
-        new CryptoPP::Base64Encoder(
-            new CryptoPP::StringSink(iv_b64), false
-        )
-    );
-  }
-  /* the server expects 24 bytes */
-  assert(iv_b64.size() == 24);
-
-  /* create the RSA Public key from the UID_PUBKEY_BYTES #define */
-  uint8_t pubkey_bytes[] = {UID_PUBKEY_BYTES};
-  CryptoPP::RSA::PublicKey publicKey;
-  CryptoPP::Integer modulus;
-  modulus.Decode(pubkey_bytes, 28);
-  publicKey.SetModulus(modulus);
-  publicKey.SetPublicExponent(65537);
-
-  /* encrypt the AES key and encode the encryptes key to base64 */
-  std::string aes_key_encrypted_base64;
-  {
-    CryptoPP::RSAES_PKCS1v15_Encryptor e( publicKey );
-    CryptoPP::StringSource s( aes_key, AES_KEY_SIZE, true,
-        new CryptoPP::PK_EncryptorFilter(rng, e,
+    /* Base64 encode IV */
+    std::string iv_b64;
+    {
+      CryptoPP::StringSource s(iv, AES_BLOCK_SIZE, true,
           new CryptoPP::Base64Encoder(
-            new CryptoPP::StringSink(aes_key_encrypted_base64), false /*insertLineBreaks*/
+              new CryptoPP::StringSink(iv_b64), false
+          )
+      );
+    }
+    /* the server expects 24 bytes */
+    assert(iv_b64.size() == 24);
+
+    /* create the RSA Public key from the UID_PUBKEY_BYTES #define */
+    uint8_t pubkey_bytes[] = {UID_PUBKEY_BYTES};
+    CryptoPP::RSA::PublicKey publicKey;
+    CryptoPP::Integer modulus;
+    modulus.Decode(pubkey_bytes, 28);
+    publicKey.Initialize(modulus, 65537);
+
+    /* encrypt the AES key and encode the encryptes key to base64 */
+    std::string aes_key_encrypted_base64;
+    {
+      CryptoPP::RSAES_PKCS1v15_Encryptor e( publicKey );
+      CryptoPP::StringSource s( aes_key, AES_KEY_SIZE, true,
+          new CryptoPP::PK_EncryptorFilter(rng, e,
+            new CryptoPP::Base64Encoder(
+              new CryptoPP::StringSink(aes_key_encrypted_base64), false /*insertLineBreaks*/
+            )
+          )
+      );
+    }
+    /* the server expects 40 bytes */
+    assert(aes_key_encrypted_base64.size() == 40);
+
+    /* now encrypt the JSON string with AES */
+    std::string json_string_encrypted_b64;
+    {
+      CryptoPP::CBC_Mode< CryptoPP::AES >::Encryption e;
+      e.SetKeyWithIV( aes_key, AES_KEY_SIZE, iv );
+      CryptoPP::StringSource s( json_string, true,
+        new CryptoPP::StreamTransformationFilter( e,
+          new CryptoPP::Base64Encoder(
+            new CryptoPP::StringSink( json_string_encrypted_b64 ), false /*insertLineBreaks*/
           )
         )
-    );
-  }
-  /* the server expects 40 bytes */
-  assert(aes_key_encrypted_base64.size() == 40);
+      );
+    }
 
-  /* now encrypt the JSON string with AES */
-  std::string json_string_encrypted_b64;
+    /* the number of padding bytes (AES can only encrypt a multiple of 16 bytes) */
+    unsigned char paddingSize = json_string.size() % 16;
+
+    {
+      /* the final bytearray consists of the
+         1 byte paddingSize
+         24 bytes IV (base64)
+         X bytes encrypted JSON (base64)
+         40 bytes encrypted AES key (base64) */
+      CryptoPP::Base64Encoder b(
+              new CryptoPP::FileSink(std::cout), false /*insertLineBreaks*/
+          );
+      b.Put(paddingSize);
+      b.Put(reinterpret_cast<const byte*>(iv_b64.c_str()), iv_b64.size());
+      b.Put(reinterpret_cast<const byte*>(json_string_encrypted_b64.c_str()), json_string_encrypted_b64.size());
+      b.Put(reinterpret_cast<const byte*>(aes_key_encrypted_base64.c_str()), aes_key_encrypted_base64.size());
+      b.MessageEnd();
+    }
+  }
+  catch(std::exception& e)
   {
-    CryptoPP::CBC_Mode< CryptoPP::AES >::Encryption e;
-    e.SetKeyWithIV( aes_key, AES_KEY_SIZE, iv );
-    CryptoPP::StringSource s( json_string, true,
-      new CryptoPP::StreamTransformationFilter( e,
-        new CryptoPP::Base64Encoder(
-          new CryptoPP::StringSink( json_string_encrypted_b64 ), false /*insertLineBreaks*/
-        )
-      )
-    );
+    std::cerr << "caught crypto exception of type " << typeid(e).name() << "\n";
+    std::cerr << "what: " << e.what() << std::endl;
+    return 1;
   }
 
-  /* the number of padding bytes (AES can only encrypt a multiple of 16 bytes) */
-  unsigned char paddingSize = json_string.size() % 16;
-
-  {
-    /* the final bytearray consists of the
-       1 byte paddingSize
-       24 bytes IV (base64)
-       X bytes encrypted JSON (base64)
-       40 bytes encrypted AES key (base64) */
-    CryptoPP::Base64Encoder b(
-            new CryptoPP::FileSink(std::cout), false /*insertLineBreaks*/
-        );
-    b.Put(paddingSize);
-    b.Put(reinterpret_cast<const byte*>(iv_b64.c_str()), iv_b64.size());
-    b.Put(reinterpret_cast<const byte*>(json_string_encrypted_b64.c_str()), json_string_encrypted_b64.size());
-    b.Put(reinterpret_cast<const byte*>(aes_key_encrypted_base64.c_str()), aes_key_encrypted_base64.size());
-    b.MessageEnd();
-  }
   return 0;
 }
