8cf1f26d0a97bf98239d95740a7768ec44629938
Martin Müllenhaupt <mm+git@netlair.de>
2017-06-08 20:32:19 +0200

    fix openssl script

--- encode_openssl_modulus.py
+++ encode_openssl_modulus.py
@@ -2,15 +2,17 @@
 # -*- coding: utf-8 -*-
 
 import sys
-if len(sys.argv) < 2:
-  sys.exit('You must provide the integer as first argument')
 
-# treat the argument as openssl output `openssl rsa -noout -inform PEM -in /path/to/pubkey.pem -pubin -modulus`
+def exit():
+  sys.exit('Argument must start with "Modulus=". Please provide as first argument the output of >openssl rsa -noout -inform PEM -in faf_pub.pem -pubin -modulus')
+
+if len(sys.argv) < 2:
+  exit()
 
 if not sys.argv[1].startswith("Modulus="):
-  sys.exit('Argument must start with "Modulus=". Please provide as first argument the output of >openssl rsa -noout -inform PEM -in /path/to/pubkey.pem -pubin -modulus')
-  
+  exit()
+
+  # treat the argument as openssl output `openssl rsa -noout -inform PEM -in /path/to/pubkey.pem -pubin -modulus`
 hex_modulus = sys.argv[1].lstrip("Modulus=")
 modulus_bytes = bytearray.fromhex(hex_modulus)
 print(','.join(map(str, modulus_bytes)))
-#print(len(modulus_bytes))
